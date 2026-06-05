fabcb76828e41b76019cf6ca16ebb8e0df84df20
SwapnilHadage <221566039+SwapnilHadage@users.noreply.github.com>
2026-06-05 17:43:13 +0530

    Initial react setup

---
+++ src/pages/Favrourites.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Favrourites() {
+  return (
+    <div>Favrourites</div>
+  )
+}
+
+export default Favrourites
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import {Home} from './Home'
+import {Favourites} from './Favourites'
+
+
+export {
+  Home, 
+  Favourites,
+  
+  
+};
